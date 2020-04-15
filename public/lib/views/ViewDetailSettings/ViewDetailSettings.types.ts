e284a265d8eb5c44f27948252e3a22bb0258d052
De Weerdt Maarten <maarten.deweerdt@cronos.be>
2020-04-15 14:31:56 +0200

    fix(red-305): redirect on cancel

--- public/lib/views/ViewDetailSettings/ViewDetailSettings.types.ts
+++ public/lib/views/ViewDetailSettings/ViewDetailSettings.types.ts
@@ -3,8 +3,16 @@ import { RouteConfigComponentProps } from '@redactie/redactie-core';
 import { ViewSchema } from '../../services/view';
 import { Tab } from '../../views.types';
 
-export interface ViewDetailSettingsProps extends RouteConfigComponentProps {
+export interface ViewDetailSettingsProps<
+	Params extends {
+		[K in keyof Params]?: string;
+	} = {}
+> extends RouteConfigComponentProps<Params> {
 	view: ViewSchema;
 	tenantId: string;
 	onSubmit: (sectionData: any, tab: Tab) => void;
+}
+
+export interface ViewDetailSettingsMatchProps {
+	siteId: string;
 }
