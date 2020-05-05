d6a4a0c9dab0c142fc6eec481735600d7ed49889
Jeroen Valcke <jeroen.valcke@studiohyperdrive.be>
2020-05-05 09:45:38 +0200

    style(formik): remove unnecessary "={true}"

--- public/lib/components/forms/FormViewOptions/FormViewOptions.tsx
+++ public/lib/components/forms/FormViewOptions/FormViewOptions.tsx
@@ -13,7 +13,7 @@ const FormViewOptions: FC<FormViewOptionsProps> = ({
 	onSubmit,
 }) => {
 	return (
-		<Formik initialValues={formState} onSubmit={onSubmit} enableReinitialize={true}>
+		<Formik initialValues={formState} onSubmit={onSubmit} enableReinitialize>
 			{() => (
 				<>
 					<AutoSubmit />
