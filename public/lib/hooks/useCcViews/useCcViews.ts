a3615a583842fe3a319f093bdf7d47ca178e025f
Jeroen Valcke <jeroen.valcke@studiohyperdrive.be>
2020-09-08 18:59:40 +0200

    fix(useCcViews): memo observables

--- public/lib/hooks/useCcViews/useCcViews.ts
+++ public/lib/hooks/useCcViews/useCcViews.ts
@@ -1,13 +1,18 @@
 import { useObservable } from '@mindspace-io/react';
+import { useMemo } from 'react';
 
 import { ViewSchema } from '../../services/views';
 import { ccViewsFacade } from '../../store/ccViews';
 import { LoadingState } from '../../views.types';
 
 const useCcViews = (key: string): [LoadingState, ViewSchema[]] => {
-	const [loading] = useObservable(ccViewsFacade.getIsFetching(key), LoadingState.Loading);
-	const [views] = useObservable(ccViewsFacade.getItemValue(key), []);
-	const [error] = useObservable(ccViewsFacade.getItemError(key), null);
+	const isFetching$ = useMemo(() => ccViewsFacade.getIsFetching(key), [key]);
+	const views$ = useMemo(() => ccViewsFacade.getItemValue(key), [key]);
+	const error$ = useMemo(() => ccViewsFacade.getItemError(key), [key]);
+
+	const [loading] = useObservable(isFetching$, LoadingState.Loading);
+	const [views] = useObservable(views$, []);
+	const [error] = useObservable(error$, null);
 
 	const loadingState = error ? LoadingState.Error : loading;
 
