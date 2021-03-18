5ce82f513dc1067b5b404ad252242b91aa1d4893
Andry Charlier <andry.charlier@studiohyperdrive.be>
2021-03-18 16:38:03 +0100

    feat(detail-config-static): show add condition form in Card

--- public/lib/views/ViewDetailConfigStatic/ViewDetailConfigStatic.tsx
+++ public/lib/views/ViewDetailConfigStatic/ViewDetailConfigStatic.tsx
@@ -1,4 +1,4 @@
-import { Button, Card } from '@acpaas-ui/react-components';
+import { Button, Card, CardBody } from '@acpaas-ui/react-components';
 import { Table } from '@acpaas-ui/react-editorial-components';
 import { useNavigate, useSiteContext, useTenantContext } from '@redactie/utils';
 import { move } from 'ramda';
@@ -213,18 +213,22 @@ const ViewConfigStatic: FC<ViewsDetailRouteProps> = ({ rights }) => {
 									Voorwaarde toevoegen
 								</Button>
 							) : (
-								<FormEditStaticCondition
-									formData={{
-										field: 'uuid',
-										index: 0,
-										value: '',
-										uuid: '',
-									}}
-									onSubmit={addCondition}
-									onCancel={() => setshowCreateConditionForm(false)}
-									submitLabel={t(CORE_TRANSLATIONS.BUTTON_ADD)}
-									submitType="success"
-								/>
+								<Card className="u-margin-top">
+									<CardBody>
+										<h3 className="h4 u-margin-bottom">Voorwaarde toevoegen</h3>
+										<FormEditStaticCondition
+											formData={{
+												field: 'uuid',
+												index: 0,
+												value: '',
+												uuid: '',
+											}}
+											onSubmit={addCondition}
+											onCancel={() => setshowCreateConditionForm(false)}
+											submitLabel={t(CORE_TRANSLATIONS.BUTTON_ADD)}
+										/>
+									</CardBody>
+								</Card>
 							)}
 						</div>
 					)}
